c695de15fae52f97f886074131e65db4202b008f
felix.eyetan <felix.eyetan@justice.gov.uk>
2023-06-17 10:59:30 +0100

    refactor configuration

--- reports/paloalto/jest.config.ts
+++ reports/paloalto/jest.config.ts
@@ -7,5 +7,8 @@ const config: Config.InitialOptions = {
     transform: {
      '^.+\\.tsx?$': 'ts-jest',
     },
+    testEnvironment: 'node',
+    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
 export default config;
