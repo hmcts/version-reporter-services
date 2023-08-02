0da49d22aeffd712d18d1700a8a7f13141b8028d
felix.eyetan <felix.eyetan@justice.gov.uk>
2023-08-02 10:56:35 +0100

    Refactor

--- pipeline-scripts/publish-image.sh
+++ pipeline-scripts/publish-image.sh
@@ -10,13 +10,7 @@ REPORT_NAME=$1
 TAG=$2
 ACR_RESOURCE_GROUP=$3
 ACR_NAME=$4
-PUBLISH_IMAGE=$5
 
-if [[ "$PUBLISH_IMAGE" == "$REPORT_NAME" || "$PUBLISH_IMAGE" == "All" ]]
-then
-  echo "Publishing a new image to '${ACR_NAME} for '${REPORT_NAME}'"
-  az acr build -r "${ACR_NAME}" -t "${TAG}" -g "${ACR_RESOURCE_GROUP}" .
-else
-  echo "No image published to '${ACR_NAME}' for ${REPORT_NAME}."
-  echo "You can manually publish your image via the pipeline 'Publish New Image' option"
-fi
+echo "Publishing a new image to '${ACR_NAME} for '${REPORT_NAME}'"
+
+az acr build -r "${ACR_NAME}" -t "${TAG}" -g "${ACR_RESOURCE_GROUP}" .
