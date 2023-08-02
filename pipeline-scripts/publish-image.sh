53ee971100b732bdf7b03696a1c5e80c9378ed83
Felix Eyetan <felix.eyetan@justice.gov.uk>
2023-08-02 11:51:17 +0100

    Reverting recent changes (#49)
    * Reverting recent changes

--- pipeline-scripts/publish-image.sh
+++ pipeline-scripts/publish-image.sh
@@ -10,21 +10,7 @@ REPORT_NAME=$1
 TAG=$2
 ACR_RESOURCE_GROUP=$3
 ACR_NAME=$4
-PUBLISH_IMAGE=$5
 
-CHANGES=$(git diff refs/heads/master..HEAD^ --name-only | grep -c "/${REPORT_NAME}/" | xargs)
+echo "Publishing a new image to '${ACR_NAME} for '${REPORT_NAME}'"
 
-if [[ "$CHANGES" -gt 0 || "$PUBLISH_IMAGE" == "$REPORT_NAME" || "$PUBLISH_IMAGE" == "All" ]]
-then
-
-  if [[ "$PUBLISH_IMAGE" == "$REPORT_NAME" || "$PUBLISH_IMAGE" == "All" ]]
-  then
-    echo "Publishing a new image to '${ACR_NAME} for '${REPORT_NAME}'"
-  else
-    echo "${CHANGES} files have been modified in '${REPORT_NAME}' report. Publishing a new image to '${ACR_NAME}"
-  fi
-
-  az acr build -r "${ACR_NAME}" -t "${TAG}" -g "${ACR_RESOURCE_GROUP}" .
-else
-  echo "${CHANGES} files have been modified in '${REPORT_NAME}' report. No image published to '${ACR_NAME}'"
-fi
+az acr build -r "${ACR_NAME}" -t "${TAG}" -g "${ACR_RESOURCE_GROUP}" .
