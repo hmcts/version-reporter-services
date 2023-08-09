44d85bb73acafb57c8d7d1f499694f3f679f829a
felix.eyetan <felix.eyetan@justice.gov.uk>
2023-08-09 15:35:00 +0100

    Update script to fix error in pod

--- reports/renovate/renovate-prs.sh
+++ reports/renovate/renovate-prs.sh
@@ -96,13 +96,15 @@ repositories=$(jq --argjson renovate "$renovate_result" --argjson updatecli "$up
 count=$(echo "$repositories" | jq '. | length')
 echo "Merged results, ${count} in total"
 
-# Define an array variable to hold all documents
-declare -a documents
-
 echo "Generate documents with verdicts for storage"
 
+# Define an array variable to hold all documents
+documents=()
+idx=1
+
 # Loop through merged documents and enhance each
-for ((idx = 0; idx < count; idx++)); do
+while [ "$idx" -lt "$count" ]
+do
   repository=$(echo "$repositories" | jq -r ".[$idx]")
 
   # The document id
@@ -149,6 +151,8 @@ for ((idx = 0; idx < count; idx++)); do
     --arg color_code "$color_code" '. + {id: $id, displayName: $display_name, verdict: $verdict, colorCode: $color_code, reportType: $report_type}')
 
   documents+=("$document")
+
+  idx=$((idx + 1))
 done
 
 # ---------------------------------------------------------------------------
