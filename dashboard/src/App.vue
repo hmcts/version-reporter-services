aa213bd8adcdac4fc48e8dcf2546e8fe758a1da1
Felix Eyetan <felix.eyetan@justice.gov.uk>
2023-08-16 11:59:59 +0100

    DTSPO-14815: Adding base files for dashboard (#66)
    * First cut adding base files for dashboard

---
+++ dashboard/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import HelloWorld from './components/Dashboard.vue'
+</script>
+
+<template>
+  <div class="vrs_header">
+    <h1>Version Reporter Dashboard</h1>
+  </div>
+  <HelloWorld msg="[WIP]"/>
+</template>
+
+<style scoped>
+.vrs_header {
+
+}
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ dashboard/src/components/Dashboard.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {ref} from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h2>{{ msg }}</h2>
+
+  <div class="card">
+    <button type="button" @click="count++">Count is {{ count }}</button>
+    <p style="font-size:3em">
+      🤓
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://github.com/hmcts/version-reporter-services/blob/master/README.md" target="_blank"
+    >the README</a>, for more detail
+  </p>
+  <p class="read-the-docs">This is currently a work-in-progress and would be updated often</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
