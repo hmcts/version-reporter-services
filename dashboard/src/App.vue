8a8de2832dae469fdcb91db8ac289e8b8d0dbffa
felix.eyetan <felix.eyetan@justice.gov.uk>
2023-08-16 11:53:28 +0100

    Baisc updated to get a default-type page going

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,29 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Dashboard.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="vrs_header">
+    <h1>Version Reporter Dashboard</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="[WIP]"/>
 </template>
 
 <style scoped>
+.vrs_header {
+
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
