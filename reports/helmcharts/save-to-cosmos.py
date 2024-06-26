bc4e59fdfb75ef415fdd3004d816ce642101b4d7
nicholas chang <nicholasc001@hotmail.com>
2024-06-26 15:43:06 +0100

    fix indendation

--- reports/helmcharts/save-to-cosmos.py
+++ reports/helmcharts/save-to-cosmos.py
@@ -24,8 +24,8 @@ def remove_documents(container):
         current_time = get_formatted_datetime()
         print(f"Removing all document added before {current_time}")
         for item in container.query_items(
-                        query = f"SELECT * FROM c WHERE c.environment = '{environment}'",
-                        enable_cross_partition_query=True):
+                query = f"SELECT * FROM c WHERE c.environment = '{environment}'",
+                enable_cross_partition_query=True):
             container.delete_item(item, partition_key=item["namespace"])
 
         print("Removing documents complete")
