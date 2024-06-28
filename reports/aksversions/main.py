af677f5f875ddb39438fd4485fac761d738f58a9
Marty Fox <martin.fox1@justice.gov.uk>
2024-06-28 12:15:19 +0100

    Adding test file with mocks for azure interaction

--- reports/aksversions/main.py
+++ reports/aksversions/main.py
@@ -1,156 +1,162 @@
-"""
-Script providing functionality to fetch information
-from Azure for AKS Clusters and calls another script to store in a cosmosdb.
-"""
-# --------
 # Import relevant packages
-# --------
 import os
 import uuid
 import json
+import logging
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import SubscriptionClient
 from azure.mgmt.containerservice import ContainerServiceClient
 from azure.cosmos import CosmosClient, exceptions
 from cosmos_functions import remove_documents, add_documents
 
+# Set up logging with a custom format
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+# Set the logging level for the Azure SDK to WARNING to suppress INFO and DEBUG logs
+logging.getLogger('azure').setLevel(logging.WARNING)
+
 # Define Functions
 def get_minor_version(version_number_str):
+    """Extracts the minor version from a version string."""
     return float(version_number_str[version_number_str.find('.') + 1: version_number_str.rfind('.')])
 
-# Define Environment variables for Cosmos
-endpoint = os.getenv("COSMOS_DB_URI", None)
-key = os.getenv("COSMOS_KEY", None)
-database = os.getenv("COSMOS_DB_NAME", "reports")
-container_name = os.getenv("COSMOS_DB_CONTAINER", "aksversions")
+def main():
+    """Main function to fetch information from Azure for AKS Clusters and store in a cosmosdb."""
+    
+    #Local Dev only
+    save_to_cosmos = os.getenv("SAVE_TO_COSMOS", 'True').lower() in ('true', '1', 't')
 
-#Local Dev only
-save_to_cosmos = os.getenv("SAVE_TO_COSMOS", 'True').lower() in ('true', '1', 't')
 
-###############
-# Script start
-###############
+    # Define Environment variables for Cosmos
+    if save_to_cosmos: 
+        endpoint = os.getenv("COSMOS_DB_URI", None)
+        key = os.getenv("COSMOS_KEY", None)
+        database = os.getenv("COSMOS_DB_NAME", "reports")
+        container_name = os.getenv("COSMOS_DB_CONTAINER", "aksversions")
 
-# Authenticate to Azure
-print("Logging into Azure...")
-credential = DefaultAzureCredential()
+        if not all([endpoint, key]):
+            logging.error("COSMOS_DB_URI and COSMOS_KEY environment variables must be set.")
+            return
 
-# Initialize the SubscriptionClient
-subscription_client = SubscriptionClient(credential)
+    # Authenticate to Azure
+    logging.info("Logging into Azure...")
+    credential = DefaultAzureCredential()
 
-# Get all subscriptions for the tenant
-subscriptions = subscription_client.subscriptions.list()
+    # Initialize the SubscriptionClient
+    subscription_client = SubscriptionClient(credential)
 
-# Initialize an empty list to hold all the cluster information
-clusters_info = []
+    # Get all subscriptions for the tenant
+    subscriptions = subscription_client.subscriptions.list()
 
-# Define your search terms
-search_terms = ['CFT', 'SHAREDSERVICES']
+    # Initialize an empty list to hold all the cluster information
+    clusters_info = []
 
-# For each subscription, get all AKS clusters
-print("Using available subscriptions to find AKS clusters...\n")
+    # Define your search terms
+    search_terms = ['CFT', 'SHAREDSERVICES']
 
-########################################################################
-# Use the list of subscriptions to find all AKS Clusters
-# subscriptions are filtered based on the search_terms so we only search for AKS clusters in subscriptions we own
-########################################################################
+    # For each subscription, get all AKS clusters
+    logging.info("Using available subscriptions to find AKS clusters...")
 
-for sub in subscriptions:
-    # Get the subscription details
-    sub_details = subscription_client.subscriptions.get(sub.subscription_id)
+    # Use the list of subscriptions to find all AKS Clusters
+    for sub in subscriptions:
+        # Get the subscription details
+        sub_details = subscription_client.subscriptions.get(sub.subscription_id)
 
-    # Check if the subscription name contains any of the search terms
-    if any(term in sub_details.display_name for term in search_terms):
-        
-        print("Checking subscription: " + sub_details.display_name + "\n")
-        
-        # Initialize the ContainerServiceClient for the subscription
-        container_client = ContainerServiceClient(credential, sub.subscription_id)
+        # Check if the subscription name contains any of the search terms
+        if any(term in sub_details.display_name for term in search_terms):
 
-        # Get all AKS clusters in the subscription
-        aks_clusters = container_client.managed_clusters.list()
+            logging.info(f"Checking subscription: {sub_details.display_name}")
 
-        # For each AKS cluster, get its current Kubernetes version and available upgrade versions
-        for cluster in aks_clusters:
-            # Extract the resource group name from the cluster id
-            resource_group_name = cluster.id.split('/')[4]
+            # Initialize the ContainerServiceClient for the subscription
+            container_client = ContainerServiceClient(credential, sub.subscription_id)
 
-            print("Found AKS cluster: " + cluster.name + "\n")
-            
-            # Get the upgrade profile for the cluster
-            upgrade_profile = container_client.managed_clusters.get_upgrade_profile(resource_group_name, cluster.name)
+            # Get all AKS clusters in the subscription
+            aks_clusters = container_client.managed_clusters.list()
 
-            print("Checking AKS cluster: " + cluster.name + " for updates...")
-            
-            # Get the current version and available updates from the upgrade profile
-            current_version = upgrade_profile.control_plane_profile.kubernetes_version
-            updates = upgrade_profile.control_plane_profile.upgrades
+            # For each AKS cluster, get its current Kubernetes version and available upgrade versions
+            for cluster in aks_clusters:
+                # Extract the resource group name from the cluster id
+                resource_group_name = cluster.id.split('/')[4]
 
-            if updates is None:
-                available_updates = "No updates available"
-                verdict = "No update required"
-                color_code = "green"
-            else:
-                for upgrade in updates:
-                    if upgrade.is_preview:
-                        available_updates = upgrade.kubernetes_version
-                        verdict = "Preview only, wait for general availability"
-                        color_code = "orange"
-                    elif ((get_minor_version(upgrade.kubernetes_version) - get_minor_version(current_version)) < 1 ):
-                        available_updates = upgrade.kubernetes_version
-                        verdict = "Patch version update only"
-                        color_code = "orange"
-                    else:
-                        available_updates = upgrade.kubernetes_version
-                        verdict = "Update"
-                        color_code = "red"
+                logging.info(f"Found AKS cluster: {cluster.name}")
 
-            print("Creating output object for: " + cluster.name + "\n")
+                # Get the upgrade profile for the cluster
+                upgrade_profile = container_client.managed_clusters.get_upgrade_profile(resource_group_name, cluster.name)
 
-            # Create a dictionary with all the cluster information
-            cluster = {
-                "id": str(uuid.uuid4()),
-                "subscription": sub_details.display_name,
-                "clusterName": cluster.name,
-                "currentVersion": current_version,
-                "upgradeableVersion": max(available_updates) if type(available_updates) is list else available_updates,
-                "colorCode": color_code,
-                "resourceType": "CFT Cluster" if "cft" in cluster.name else "SS Cluster",
-                "powerState": cluster.power_state.code if cluster.power_state else "Unknown",
-                "verdict": verdict
-            }
+                logging.info(f"Checking AKS cluster: {cluster.name} for updates...")
 
-            # Add the dictionary to the list
-            clusters_info.append(cluster)
+                # Get the current version and available updates from the upgrade profile
+                current_version = upgrade_profile.control_plane_profile.kubernetes_version
+                updates = upgrade_profile.control_plane_profile.upgrades
 
-# Print the list of all cluster information as a JSON object
-print(f"Number of AKS Clusters found: {len(clusters_info)}.\n")
+                if updates is None:
+                    available_updates = "No updates available"
+                    verdict = "No update required"
+                    color_code = "green"
+                else:
+                    for upgrade in updates:
+                        if upgrade.is_preview:
+                            available_updates = upgrade.kubernetes_version
+                            verdict = "Preview only, wait for general availability"
+                            color_code = "orange"
+                        elif ((get_minor_version(upgrade.kubernetes_version) - get_minor_version(current_version)) < 1 ):
+                            available_updates = upgrade.kubernetes_version
+                            verdict = "Patch version update only"
+                            color_code = "orange"
+                        else:
+                            available_updates = upgrade.kubernetes_version
+                            verdict = "Update"
+                            color_code = "red"
 
-if not save_to_cosmos:
-    print(json.dumps(clusters_info, indent=4))
+                logging.info(f"Creating output object for: {cluster.name}")
 
-########################################################################
-# We have all the AKS clusters now saved in clusters_info
-# Now we need to store them in CosmosDB and the aksversions container
-########################################################################
+                # Create a dictionary with all the cluster information
+                cluster = {
+                    "id": str(uuid.uuid4()),
+                    "subscription": sub_details.display_name,
+                    "clusterName": cluster.name,
+                    "currentVersion": current_version,
+                    "upgradeableVersion": max(available_updates) if type(available_updates) is list else available_updates,
+                    "colorCode": color_code,
+                    "resourceType": "CFT Cluster" if "cft" in cluster.name else "SS Cluster",
+                    "powerState": cluster.power_state.code if cluster.power_state else "Unknown",
+                    "verdict": verdict
+                }
 
-if save_to_cosmos:
-# Establish connection to cosmos db
-    cosmosClient = CosmosClient(endpoint, credential=key)
+                # Add the dictionary to the list
+                clusters_info.append(cluster)
 
-# Save documents to cosmos db
-    try:
-        database = cosmosClient.get_database_client(database)
-        db_container = database.get_container_client(container_name)
+    # Print the list of all cluster information as a JSON object
+    logging.info(f"Number of AKS Clusters found: {len(clusters_info)}.")
 
-        remove_documents(db_container)
-        add_documents(db_container, clusters_info)
+    if not save_to_cosmos:
+        logging.info(json.dumps(clusters_info, indent=4))
 
-    except AttributeError as attribute_error:
-        print(f"Saving to db failed with AttributeError error: {attribute_error}")
-        raise
-    except exceptions.CosmosHttpResponseError as http_response_error:
-        print(f"Saving to db failed with CosmosHttpResponseError error: {http_response_error}")
-        raise
+    # We have all the AKS clusters now saved in clusters_info
+    # Now we need to store them in CosmosDB and the aksversions container
+    if save_to_cosmos:
+        # Establish connection to cosmos db
+        cosmosClient = CosmosClient(endpoint, credential=key)
 
-    print("Save to database completed.")
+        # Save documents to cosmos db
+        try:
+            database = cosmosClient.get_database_client(database)
+            db_container = database.get_container_client(container_name)
+
+            remove_documents(db_container)
+            add_documents(db_container, clusters_info)
+
+        except AttributeError as attribute_error:
+            logging.error(f"Saving to db failed with AttributeError error: {attribute_error}")
+            raise
+        except exceptions.CosmosHttpResponseError as http_response_error:
+            logging.error(f"Saving to db failed with CosmosHttpResponseError error: {http_response_error}")
+            raise
+
+        logging.info("Save to database completed.")
+
+if __name__ == "__main__":
+    main()
---
+++ reports/aksversions/test_main.py
@@ -0,0 +1,52 @@
+import pytest
+import os
+from unittest.mock import patch, MagicMock
+from main import get_minor_version, main
+
+def test_get_minor_version():
+    assert get_minor_version("1.2.3") == 2.0
+    assert get_minor_version("4.5.6") == 5.0
+
+@patch('main.DefaultAzureCredential')
+@patch('main.SubscriptionClient')
+@patch('main.ContainerServiceClient')
+@patch('main.CosmosClient')
+def test_main_script(mock_cosmos_client, mock_container_service_client, mock_subscription_client, mock_default_credential):
+
+    os.environ['COSMOS_DB_URI'] = 'URI'
+    os.environ['COSMOS_KEY'] = 'KEY'
+
+    # Mock the Azure credential
+    mock_default_credential.return_value = MagicMock()
+
+    # Mock the SubscriptionClient
+    mock_subscription = MagicMock()
+    mock_subscription.subscriptions.list.return_value = [mock_subscription]
+    mock_subscription.subscriptions.get.return_value = MagicMock(display_name='CFT')
+    mock_subscription_client.return_value = mock_subscription
+
+    # Mock the ContainerServiceClient
+    mock_container_service = MagicMock()
+    mock_container_service.managed_clusters.list.return_value = []
+    mock_container_service_client.return_value = mock_container_service
+
+    # Mock the CosmosClient
+    mock_cosmos = MagicMock()
+    mock_cosmos.get_database_client.return_value = MagicMock()
+    mock_cosmos_client.return_value = mock_cosmos
+
+    # Call your main script function here
+    main()
+
+    # Assert that the mocks were called as expected
+    mock_default_credential.assert_called_once()
+
+    mock_subscription_client.assert_called_once_with(mock_default_credential.return_value)
+    mock_subscription_client.return_value.subscriptions.get.assert_called_once_with(mock_subscription_client.return_value.subscription_id)
+    assert 'CFT' in mock_subscription_client.return_value.subscriptions.get.return_value.display_name
+
+    mock_container_service_client.assert_called_once_with(mock_default_credential.return_value, mock_subscription_client.return_value.subscription_id)
+    mock_container_service.managed_clusters.list.assert_called_once()
+
+    mock_cosmos_client.assert_called_once_with(os.environ['COSMOS_DB_URI'], credential=os.environ['COSMOS_KEY'])
+    mock_cosmos.get_database_client.assert_called_once()
