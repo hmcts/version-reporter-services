8a4c12052e33897074064929eadf24caa12247d9
Marty Fox <martin.fox1@justice.gov.uk>
2024-07-01 15:33:56 +0100

    Move to specific KV policy for sbox MI, to limit issues with for_each and unknown values

--- components/versionreporting/06-ptlsbox-mi-access.tf
+++ components/versionreporting/06-ptlsbox-mi-access.tf
@@ -14,6 +14,29 @@ resource "azurerm_user_assigned_identity" "ptlsbox_managed_identity" {
   tags = local.common_tags
 }
 
+resource "azurerm_key_vault_access_policy" "ptlsbox_managed_identity_access_policy" {
+  key_vault_id = module.version_reporter_key_vault.key_vault_id
+
+  object_id = azurerm_user_assigned_identity.ptlsbox_managed_identity.principal_id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+
+  key_permissions = [
+    "Get",
+    "List",
+  ]
+
+  certificate_permissions = [
+    "Get",
+    "List",
+  ]
+
+  secret_permissions = [
+    "Get",
+    "List",
+  ]
+
+}
+
 # Service connection does not have enough access to grant this via automation
 # The addition of the MI to the group has been completed manually and the code commented here to limit failures
 # The code is being left here for reference and understand if required in future
