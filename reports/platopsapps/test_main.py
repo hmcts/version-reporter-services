794257c54af98bea34b52c4c6fecc79548292de4
Rees Pozzi <reespozzi@icloud.com>
2024-07-03 13:56:15 +0100

    Adds tests

--- reports/platopsapps/test_main.py
+++ reports/platopsapps/test_main.py
@@ -24,8 +24,12 @@ def test_get_camunda_version(mock_kube_client):
     mock_kube_client.list_namespaced_pod.return_value.items = [
         MagicMock(metadata=MagicMock(name="test-pod"))
     ]
-    mock_kube_client.read_namespaced_pod_log.return_value = "Camunda Platform: (v7.21.0-ee)"
-    
+    mock_kube_client.read_namespaced_pod_log.return_value = """
+        This is a random test
+        There would be many many more lines of very useful logs
+        Camunda Platform: (v7.21.0-ee)
+        More lines...
+    """
     version = get_camunda_version("test-deployment", "test-ns")
     assert version == "v7.21.0-ee"
 
@@ -35,7 +39,11 @@ def test_get_docmosis_version(mock_kube_client):
     mock_kube_client.list_namespaced_pod.return_value.items = [
         MagicMock(metadata=MagicMock(name="test-pod"))
     ]
-    mock_kube_client.read_namespaced_pod_log.return_value = "03 Jul 2024 12:30:56,190 [localhost-startStop-1] INFO  SystemManager - Docmosis version [4.4.1_8366] initialising"
+    mock_kube_client.read_namespaced_pod_log.return_value = """
+        These are my Docmosis pod logs
+        03 Jul 2024 12:30:56,190 [localhost-startStop-1] INFO  SystemManager - Docmosis version [4.4.1_8366] initialising
+        They are used for testing this function
+    """
     
     version = get_docmosis_version("test-deployment", "test-ns")
     assert version == "4.4.1_8366"
