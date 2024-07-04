d6c2e4035dfc93e6889ce2763033c433a3628629
Marty Fox <martin.fox1@justice.gov.uk>
2024-07-04 10:27:07 +0100

    spltting tests into specific files as pytest will run them all together anyway

--- reports/platopsapps/test_main.py
+++ reports/platopsapps/test_main.py
@@ -2,7 +2,6 @@ import pytest
 import re
 from unittest.mock import patch, MagicMock
 from main import get_pod_logs, get_current_camunda_version, get_current_docmosis_version, get_current_flux_version
-from version_utility import flux_latest_version, camunda_latest_version, docmosis_latest_version, compare_versions, get_semvar, get_major_version, get_minor_version, get_patch_version
 
 # Replace k8s client with mocked object
 @pytest.fixture
@@ -57,57 +56,3 @@ def test_get_flux_version(mock_kube_client):
     
     version = get_current_flux_version()
     assert version == "2.3.1"
-
-# Test we can retrieve the version of flux from a namespace label
-def test_get_flux_version(mock_kube_client):
-    mock_namespace = MagicMock(metadata=MagicMock(labels={"app.kubernetes.io/version": "2.3.1"}))
-    mock_kube_client.read_namespace.return_value = mock_namespace
-    
-    version = get_current_flux_version()
-    assert version == "2.3.1"
-
-def test_get_semvar():
-    version = get_semvar("v7.21.0-ee")
-    assert version == "7.21.0"
-
-def test_get_patch_version():
-    version = get_patch_version("7.21.0")
-    assert version == 0
-
-def test_get_minor_version():
-    version = get_minor_version("7.21.0")
-    assert version == 21
-
-def test_get_major_version():
-    version = get_major_version("7.21.0")
-    assert version == 7
-
-def test_compare_versions():
-    result = compare_versions("7.21.0", "7.21.5", "test")
-    assert result == { 'reason': "Patch versions are different", 'colorCode': "orange", 'verdict': "review" }
-    
-def test_flux_latest_version():
-    semver_regex = r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$"
-    latest_version = flux_latest_version()
-
-    assert latest_version is not None
-    assert re.match(semver_regex, latest_version), "Returned value does not match semantic version regex"
-
-def test_camunda_latest_version():
-    semver_regex = r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$"
-    latest_version = camunda_latest_version()
-
-    assert latest_version is not None
-    assert re.match(semver_regex, latest_version), "Returned value does not match semantic version regex"
-
-
-def test_docmosis_latest_version():
-    semver_regex = r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$"
-    latest_version = docmosis_latest_version()
-
-    assert latest_version is not None
-    assert re.match(semver_regex, latest_version), "Returned value does not match semantic version regex"
-
-
-
-# flux_latest_version, camunda_latest_version, docmosis_latest_version,
