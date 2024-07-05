5453080642b882eb4ac6e6ab71a5de0183ce6ef7
Rees Pozzi <reespozzi@icloud.com>
2024-07-05 11:32:22 +0100

    Update access policy naming

--- components/managedidentity/04-managed-identities.tf
+++ components/managedidentity/04-managed-identities.tf
@@ -11,7 +11,7 @@ resource "azurerm_user_assigned_identity" "managed_identity" {
   tags = local.common_tags
 }
 
-resource "azurerm_key_vault_access_policy" "ptlsbox_managed_identity_access_policy" {
+resource "azurerm_key_vault_access_policy" "managed_identity_access_policy" {
   provider     = azurerm.ptl
   key_vault_id = data.azurerm_key_vault.ptl
   object_id    = azurerm_user_assigned_identity.managed_identity.principal_id
