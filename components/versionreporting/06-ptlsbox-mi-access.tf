9a3584e94d14182198e77d3044cfa032456c56a1
Nicholas Chang <60232417+NickAzureDevops@users.noreply.github.com>
2024-07-05 17:17:50 +0100

    Update components/versionreporting/06-ptlsbox-mi-access.tf
    Co-authored-by: Rees Pozzi <rees.pozzi@justice.gov.uk>

--- components/versionreporting/06-ptlsbox-mi-access.tf
+++ components/versionreporting/06-ptlsbox-mi-access.tf
@@ -37,7 +37,7 @@ resource "azurerm_key_vault_access_policy" "ptlsbox_managed_identity_access_poli
 
 }
 
-resource "azurerm_role_assignment" "reader" {
+resource "azurerm_role_assignment" "key_vault_contributor" {
   for_each             = local.service_principal_ids
   scope                = module.version_reporter_key_vault.key_vault_id
   role_definition_name = "Key Vault Contributor"
