4c2a2e6828d104ecf55d063aefc296b24bf16b2b
nicholas chang <nicholasc001@hotmail.com>
2024-07-05 15:17:02 +0100

    fix issue with sc

---
+++ components/versionreporting/10-local.tf
@@ -0,0 +1,13 @@
+locals {
+  service_principal_ids = [
+    "DCD-CFTAPPS-SBOX",
+    "DCD-CFTAPPS-DEV",
+    "DCD-CFTAPPS-DEMO",
+    "DCD-CFTAPPS-ITHC",
+    "DTS-CFTSBOX-INTSVC",
+    "DCD-CFTAPPS-TEST",
+    "DCD-CFTAPPS-STG",
+    "DTS-CFTPTL-INTSVC",
+    "DCD-CFTAPPS-PROD",
+  ]
+}
