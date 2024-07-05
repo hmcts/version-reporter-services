244288fcc163217c1ce5940739e98ee48492b614
Rees Pozzi <reespozzi@icloud.com>
2024-07-05 12:52:41 +0100

    Reference kv id porperly

--- components/managedidentity/04-managed-identities.tf
+++ components/managedidentity/04-managed-identities.tf
@@ -15,7 +15,7 @@ resource "azurerm_user_assigned_identity" "managed_identity" {
 
 resource "azurerm_key_vault_access_policy" "managed_identity_access_policy" {
   provider     = azurerm.ptl
-  key_vault_id = data.azurerm_key_vault.ptl
+  key_vault_id = data.azurerm_key_vault.ptl.id
   object_id    = azurerm_user_assigned_identity.managed_identity.principal_id
   tenant_id    = data.azurerm_client_config.current.tenant_id
 
