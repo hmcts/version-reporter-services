f044f8d1a9ff5da4143347ca12b1f7c241047b78
Rees Pozzi <reespozzi@icloud.com>
2024-07-15 11:49:22 +0100

    Revert "Remove infra as code"
    This reverts commit 746a488e2f76774d7540b7787ee7e0e520616c88.

--- components/storage/05-cosmosdb.tf
+++ components/storage/05-cosmosdb.tf
@@ -1,73 +1,73 @@
 # The resource group
-# resource "azurerm_resource_group" "this" {
-#   name     = format("sds-%s-%s-rg", var.service_name, var.env)
-#   location = var.location
-#   tags     = local.common_tags
-# }
+resource "azurerm_resource_group" "this" {
+  name     = format("sds-%s-%s-rg", var.service_name, var.env)
+  location = var.location
+  tags     = local.common_tags
+}
 
-# # The cosmosdb account
-# resource "azurerm_cosmosdb_account" "this" {
-#   name                      = format("%s-%s", var.product, var.service_name)
-#   location                  = var.location
-#   resource_group_name       = azurerm_resource_group.this.name
-#   kind                      = "GlobalDocumentDB"
-#   offer_type                = "Standard"
-#   enable_automatic_failover = true
+# The cosmosdb account
+resource "azurerm_cosmosdb_account" "this" {
+  name                      = format("%s-%s", var.product, var.service_name)
+  location                  = var.location
+  resource_group_name       = azurerm_resource_group.this.name
+  kind                      = "GlobalDocumentDB"
+  offer_type                = "Standard"
+  enable_automatic_failover = true
 
-#   consistency_policy {
-#     consistency_level = "Session"
-#   }
+  consistency_policy {
+    consistency_level = "Session"
+  }
 
-#   geo_location {
-#     location          = var.location
-#     failover_priority = 0
-#     zone_redundant    = true
-#   }
-#   tags = local.common_tags
-# }
+  geo_location {
+    location          = var.location
+    failover_priority = 0
+    zone_redundant    = true
+  }
+  tags = local.common_tags
+}
 
-# # The sql database
-# resource "azurerm_cosmosdb_sql_database" "this" {
-#   name                = "reports"
-#   resource_group_name = azurerm_resource_group.this.name
-#   account_name        = azurerm_cosmosdb_account.this.name
+# The sql database
+resource "azurerm_cosmosdb_sql_database" "this" {
+  name                = "reports"
+  resource_group_name = azurerm_resource_group.this.name
+  account_name        = azurerm_cosmosdb_account.this.name
 
-#   autoscale_settings {
-#     max_throughput = var.max_throughput
-#   }
-# }
+  autoscale_settings {
+    max_throughput = var.max_throughput
+  }
+}
 
-# # The report containers. One container per report
-# resource "azurerm_cosmosdb_sql_container" "this" {
-#   for_each              = var.containers_partitions
-#   name                  = each.key
-#   resource_group_name   = azurerm_resource_group.this.name
-#   account_name          = azurerm_cosmosdb_account.this.name
-#   database_name         = azurerm_cosmosdb_sql_database.this.name
-#   partition_key_path    = each.value
-#   partition_key_version = 2
+# The report containers. One container per report
+resource "azurerm_cosmosdb_sql_container" "this" {
+  for_each              = var.containers_partitions
+  name                  = each.key
+  resource_group_name   = azurerm_resource_group.this.name
+  account_name          = azurerm_cosmosdb_account.this.name
+  database_name         = azurerm_cosmosdb_sql_database.this.name
+  partition_key_path    = each.value
+  partition_key_version = 2
 
-#   autoscale_settings {
-#     max_throughput = var.max_throughput
-#   }
+  autoscale_settings {
+    max_throughput = var.max_throughput
+  }
 
-#   indexing_policy {
-#     indexing_mode = "consistent"
+  indexing_policy {
+    indexing_mode = "consistent"
 
-#     included_path {
-#       path = "/*"
-#     }
-#   }
-# }
+    included_path {
+      path = "/*"
+    }
+  }
+}
 
-# /*
-#  * Granting Cosmos DB Built-in Data Contributor to enable read/write permissions to MI
-#  */
-# resource "azurerm_cosmosdb_sql_role_assignment" "this" {
-#   resource_group_name = azurerm_cosmosdb_account.this.resource_group_name
-#   account_name        = azurerm_cosmosdb_account.this.name
-#   # Cosmos DB Built-in Data Contributor
-#   role_definition_id = "${azurerm_cosmosdb_account.this.id}/sqlRoleDefinitions/00000000-0000-0000-0000-000000000002"
-#   principal_id       = azurerm_user_assigned_identity.this.principal_id
-#   scope              = azurerm_cosmosdb_account.this.id
-# }
+/*
+ * Granting Cosmos DB Built-in Data Contributor to enable read/write permissions to MI
+ */
+resource "azurerm_cosmosdb_sql_role_assignment" "this" {
+  resource_group_name = azurerm_cosmosdb_account.this.resource_group_name
+  account_name        = azurerm_cosmosdb_account.this.name
+  # Cosmos DB Built-in Data Contributor
+  role_definition_id = "${azurerm_cosmosdb_account.this.id}/sqlRoleDefinitions/00000000-0000-0000-0000-000000000002"
+  principal_id       = azurerm_user_assigned_identity.this.principal_id
+  scope              = azurerm_cosmosdb_account.this.id
+}
