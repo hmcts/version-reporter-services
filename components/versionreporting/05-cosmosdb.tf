c89746650872ec1dd792d6df5418b0f856ba4c90
Rees Pozzi <rees.pozzi@justice.gov.uk>
2024-07-29 11:06:52 +0100

    Update role given

--- components/versionreporting/05-cosmosdb.tf
+++ components/versionreporting/05-cosmosdb.tf
@@ -71,7 +71,7 @@ data "azuread_service_principals" "pipeline" {
 resource "azurerm_role_assignment" "rbac_admin" {
   for_each = { for sp in data.azuread_service_principals.pipeline.service_principals : sp.object_id => sp }
   # Needs to have permission to Microsoft.DocumentDB/databaseAccounts/sqlRoleAssignments/write
-  role_definition_name = "DocumentDB Account Contributor"
+  role_definition_name = "Contributor"
   principal_id         = each.key
   scope                = azurerm_cosmosdb_account.this.id
 }
