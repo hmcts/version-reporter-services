298e83a1e977d150e7d441b7c6297c0da3538a57
nicholas chang <nicholasc001@hotmail.com>
2024-07-30 10:27:43 +0100

    remove output file

--- components/versionreporting/09-outputs.tf
+++ components/versionreporting/09-outputs.tf
@@ -5,7 +5,3 @@ output "cosmosdb_endpoint" {
 output "cosmosdb_url" {
   value = "${azurerm_cosmosdb_account.this.id}.documents.azure.com"
 }
-
-output "pipeline_service_principals" {
-  value = data.azuread_service_principals.pipeline.display_names
-}
