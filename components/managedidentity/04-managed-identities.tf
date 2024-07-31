83fbf78d0efea4988da2feb7d1c84de9e757b3de
nicholas chang <nicholasc001@hotmail.com>
2024-07-31 11:24:10 +0100

    rebase managed identity from master

--- components/managedidentity/04-managed-identities.tf
+++ components/managedidentity/04-managed-identities.tf
@@ -79,18 +79,3 @@ resource "azurerm_cosmosdb_sql_role_assignment" "monitoring_mi_assignment" {
 #   group_object_id  = data.azuread_group.reader_access_group.id
 #   member_object_id = azurerm_user_assigned_identity.ptlsbox_managed_identity.principal_id
 # }
-
-
-# Service connection does not have enough access to grant this via automation
-# The addition of the MI to the group has been completed manually and the code commented here to limit failures
-# The code is being left here for reference and understand if required in future
-
-# data "azuread_group" "reader_access_group" {
-#   display_name     = "DTS Readers (mg:HMCTS)"
-#   security_enabled = true
-# }
-
-# resource "azuread_group_member" "group_membership" {
-#   group_object_id  = data.azuread_group.reader_access_group.id
-#   member_object_id = azurerm_user_assigned_identity.ptlsbox_managed_identity.principal_id
-# }
