c00b0fcdd7e0eb3ed02ea62ab75a2d5cc5fd67bd
nicholas chang <nicholasc001@hotmail.com>
2024-07-31 13:48:27 +0100

    updated provider for montioring mi

--- components/managedidentity/04-managed-identities.tf
+++ components/managedidentity/04-managed-identities.tf
@@ -58,7 +58,7 @@ data "azurerm_cosmosdb_account" "pipeline_metrics" {
 }
 
 resource "azurerm_cosmosdb_sql_role_assignment" "monitoring_mi_assignment" {
-  provider            = azurerm.ptl
+  provider            = azurerm.pipeline-metrics
   resource_group_name = data.azurerm_cosmosdb_account.pipeline_metrics.resource_group_name
   account_name        = data.azurerm_cosmosdb_account.pipeline_metrics.name
   role_definition_id  = "${data.azurerm_cosmosdb_account.pipeline_metrics.id}/sqlRoleDefinitions/00000000-0000-0000-0000-000000000002"
