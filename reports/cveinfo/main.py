a7402b5dd6b71d6e21b37539d9be578a51704488
Felix Eyetan <peyetan@yahoo.com>
2024-10-13 18:59:28 +0100

    Update dding async db feature

--- reports/cveinfo/main.py
+++ reports/cveinfo/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import datetime
 import json
 import os
 import pathlib
@@ -7,20 +8,15 @@ import time
 import uuid
 from pathlib import Path
 
-from azure.cosmos import exceptions
-from azure.cosmos.aio import CosmosClient
-from azure.identity import DefaultAzureCredential
 from git import Repo
 from humanfriendly import format_timespan
 
-MAX_BATCH_SIZE = 500
-DB_URL = os.environ.get("COSMOS_DB_URI", None)
-DATABASE_NAME = os.environ.get("COSMOS_DB_NAME", "reports")
-KEY = os.environ.get("COSMOS_KEY")
-CONTAINER_NAME = os.environ.get("COSMOS_DB_CONTAINER", "cveinfo")
+from save_to_db import add_batch
+
+MAX_BATCH_SIZE = 1000
 
 
-# Utility used to cherry pick cve object
+# Utility used to cheery pick cve object
 def extract_cve_data(cve_data):
     data = dict()
     data['id'] = f'{uuid.uuid4()}'
@@ -37,14 +33,19 @@ def extract_cve_data(cve_data):
     return data
 
 
-async def create_all_the_items(container, batch, resource_id):
-    await asyncio.wait(
-        [asyncio.create_task(container.create_item(item)) for item in batch]
-    )
-    print(f"[{resource_id}][DB] Batch of {len(batch)} items done!")
+def get_year():
+    today = datetime.datetime.now()
+    return today.strftime("%Y")
 
 
-async def main():
+def filter_by_year(cve_filename):
+    pattern = f"/{get_year()}/"
+    if pattern in str(cve_filename):
+        return True
+    return False
+
+
+async def load_cve():
     start_time = time.time()
 
     # get the current working directory
@@ -57,69 +58,59 @@ async def main():
 
     # Clone the cve repo to local_dir
     repo = Repo.clone_from(repo_url, local_dir)
-    print(f"Successfully cloned  repo: {repo_url}")
+    print(f"Successfully cloned repo: {repo_url}")
 
     # Get path to cve files
     cve_dir = os.path.join(current_working_directory, "cverepo", "cves")
+    try:
+        # Sanity check there is a cves folder
+        if os.path.isdir(cve_dir):
+            print(f"The cve repository at {cve_dir} does exit")
+            batch = []
 
-    # Sanity check there is a cves folder
-    if os.path.isdir(cve_dir):
-        print(f"The cve repository at {cve_dir} does exit")
-        batch_size = 0
-        batch = []
+            # Recursively walk through all json files in cve file name format
+            files = [f for f in pathlib.Path(cve_dir).glob("**/CVE-*.json")]
+            print(f"Total of {len(files)} cve files")
 
-        # Recursively walk through all json files in cve file name format
-        files = [f for f in pathlib.Path(cve_dir).glob("**/CVE-*.json")]
-        print(f"Total of {len(files)} cve files to be processed")
+            # files = list(filter(filter_by_year, files))
+            # print(f"Total of {len(files)} cve files after filter")
 
-        # Setup Azure credential
-        credential = DefaultAzureCredential()
+            for file in files:
+                with open(file, mode='r') as cve:
+                    # Pick a few properties out, we don't need the whole lot
+                    data = json.load(cve)
+                    data = extract_cve_data(data)
 
-        try:
-            # Setup cosmosdb client for asynchronous write
-            async with CosmosClient(url=DB_URL, credential=KEY) as client:
-                db = client.get_database_client(DATABASE_NAME)
-                container = db.get_container_client(CONTAINER_NAME)
+                    # Add to a batch
+                    batch.append(data)
 
-                for file in files:
-                    with open(file, 'r') as cve:
-                        content = json.load(cve)
-                        # Pick a few properties out, we don't need the whole lot
-                        # Add to a batch list of MAX_BATCH_SIZE
-                        data = extract_cve_data(content)
-                        batch.append(data)
-                        batch_size += 1
+                    # When batch is 'full' save to db and reset counter
+                    # Sending in batches reduces payload volume of writes to db
+                    if len(batch) == MAX_BATCH_SIZE:
+                        await add_batch(batch)
+                        print(f"Saved batch of {MAX_BATCH_SIZE}")
+                        batch = []
 
-                        # When batch is 'full' save to db and resent counters
-                        # Sending in batches reduces payload volume of writes to db
-                        if batch_size == MAX_BATCH_SIZE:
-                            print(f"Reached batch of {batch_size}")
-                            await asyncio.wait(
-                                [asyncio.create_task(container.create_item(item) for item in batch)]
-                            )
-                            print(f"Saved batch of {batch_size}")
-                            batch_size = 0
-                            batch = []
-                            print(f"Reset batch to {len(batch)}")
-
-        except exceptions.CosmosResourceNotFoundError as e:
-            print(f"Error adding batch: {e}")
-        except exceptions.CosmosResourceExistsError as e:
-            print(f"Error adding batch: {e}")
-        except exceptions.CosmosHttpResponseError as e:
-            print(f"Error adding batch: {e}")
-        except exceptions.CosmosClientTimeoutError as e:
-            print(f"Error adding batch: {e}")
-
-    else:
-        print(f"The cve repository ar {cve_dir} does not exit")
-
-    # Removing directory, not massively relevant as job will exist, just due diligence
-    shutil.rmtree(local_dir, ignore_errors=True)
+            # Save remaining batched items, at some point it would be less than MAX_BATCH_SIZE
+            if len(batch) > 0:
+                print(f"Saved remaining batch of {len(batch)}")
+                await add_batch(batch)
+                # Free allocated space for batch
+                batch = []
+        else:
+            print(f"The cve repository ar {cve_dir} does not exit")
+    finally:
+        # Removing directory, not massively relevant as job will exist, just due diligence
+        shutil.rmtree(local_dir, ignore_errors=True)
+        print(f"Removed the cve repository")
 
     # Log how log it took
     elapsed_time = time.time() - start_time
     print(f'Elapsed time: {format_timespan(elapsed_time)}')
+
+
+async def main():
+    await load_cve()
 
 
 if __name__ == "__main__":
--- reports/cveinfo/save_to_db.py
+++ reports/cveinfo/save_to_db.py
@@ -9,32 +9,33 @@ from azure.cosmos.aio import CosmosClient
 endpoint = os.getenv("COSMOS_DB_URI")
 key = os.getenv("COSMOS_KEY")
 database_name = os.getenv("COSMOS_DB_NAME", "reports")
-container_name = os.getenv("COSMOS_DB_CONTAINER", "netflow")
+container_name = os.getenv("COSMOS_DB_CONTAINER", "cveinfo")
 
 
-async def add_batch(batch, resource_id):
+async def add_batch(batch):
     try:
         async with CosmosClient(url=endpoint, credential=key) as client:
             database = client.get_database_client(database_name)
             container = database.get_container_client(container_name)
 
             timer = time.time()
-            print("Starting Concurrent Batched Item Creation.")
-            await create_all_the_items(container, batch, resource_id)
+            print(f"Starting Concurrent Batched Item Creation: {timer}].")
+            await create_all_the_items(container, batch)
 
             concurrent_batch_time = time.time() - timer
             print(f"Time taken: {concurrent_batch_time:.2f} sec")
+            time.sleep(2.5)
     except exceptions.CosmosResourceNotFoundError as e:
-        print(f"Error adding batch: {batch}\n Error: {e}")
+        print(f"Error adding batch: Error: {e}")
     except exceptions.CosmosResourceExistsError as e:
-        print(f"Error adding batch: {batch}\n Error: {e}")
+        print(f"Error adding batch: Error: {e}")
     except exceptions.CosmosHttpResponseError as e:
-        print(f"Error adding batch: {batch}\n Error: {e}")
+        print(f"Error adding batch: Error: {e}")
     except exceptions.CosmosClientTimeoutError as e:
-        print(f"Error adding batch: {batch}\n Error: {e}")
+        print(f"Error adding batch: Error: {e}")
 
 
-async def create_all_the_items(container, batch, resource_id):
+async def create_all_the_items(container, batch):
     await asyncio.wait(
         [asyncio.create_task(container.create_item(item)) for item in batch]
     )
