2c0d98e6809511be65f5bc782651dc3c2b026539
Felix Eyetan <peyetan@yahoo.com>
2024-11-08 13:58:11 +0000

    update code config maxsize

--- reports/cveinfo/main.py
+++ reports/cveinfo/main.py
@@ -13,7 +13,7 @@ from humanfriendly import format_timespan
 
 from save_to_db import add_batch
 
-MAX_BATCH_SIZE = 500
+MAX_BATCH_SIZE = container_name = os.getenv("MAX_BATCH_SIZE", 1000)
 
 
 # Utility used to cheery pick cve object
@@ -21,6 +21,7 @@ def extract_cve_data(cve_data):
     data = dict()
     data['id'] = f'{uuid.uuid4()}'
     data['cveId'] = cve_data.get('cveMetadata').get('cveId')
+    data['state'] = cve_data.get('cveMetadata').get('state')
     data['dataType'] = cve_data.get('dataType')
     data['dataVersion'] = cve_data.get('dataVersion')
     data['assignerShortName'] = cve_data.get('cveMetadata').get('assignerShortName')
@@ -54,11 +55,14 @@ async def load_cve():
 
     # CVE project repo. Publicly accessible for use
     repo_url = "https://github.com/CVEProject/cvelistV5.git"
-    print(f"Cloning repo: {repo_url}")
+    print(f'Cloning repo: {repo_url}')
 
     # Clone the cve repo to local_dir
-    repo = Repo.clone_from(repo_url, local_dir)
-    print(f"Successfully cloned repo: {repo_url}")
+    envs = dict()
+    envs['sb'] = "--single-branch"
+    repo = Repo.clone_from(repo_url, local_dir, env=envs)
+    if repo:
+        print(f'Successfully cloned repo: {repo_url}')
 
     # Get path to cve files
     cve_dir = os.path.join(current_working_directory, "cverepo", "cves")
--- reports/cveinfo/save_to_db.py
+++ reports/cveinfo/save_to_db.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import time
+from datetime import datetime as dt
 
 from azure.cosmos import exceptions
 from azure.cosmos.aio import CosmosClient
@@ -12,6 +13,11 @@ database_name = os.getenv("COSMOS_DB_NAME", "reports")
 container_name = os.getenv("COSMOS_DB_CONTAINER", "cveinfo")
 
 
+def get_formatted_time():
+    now = dt.now()
+    return now.strftime("%I:%M:%S %p")
+
+
 async def add_batch(batch):
     try:
         async with CosmosClient(url=endpoint, credential=key) as client:
@@ -19,7 +25,7 @@ async def add_batch(batch):
             container = database.get_container_client(container_name)
 
             timer = time.time()
-            print(f"Starting Concurrent Batched Item Creation: {timer}].")
+            print(f"Starting Concurrent Batched Item Creation: {get_formatted_time()}.")
             await create_all_the_items(container, batch)
 
             concurrent_batch_time = time.time() - timer
@@ -35,23 +41,8 @@ async def add_batch(batch):
         print(f"Error adding batch: Error: {e}")
 
 
-def create_or_update(container, item):
-    cve_id = item.get('cveId')
-    document = container.query_items(
-        query='SELECT * FROM c WHERE c.cveId = @cveId',
-        parameters=[dict(name='@cveId', value=cve_id)],
-        enable_cross_partition_query=True)
-
-    if document:
-        item.pop('id', None)
-        document.update(item)
-        container.upsert_item(body=document)
-    else:
-        container.upsert_item(body=document)
-
-
 async def create_all_the_items(container, batch):
     await asyncio.wait(
-        [asyncio.create_task(create_or_update(container, item)) for item in batch]
+        [asyncio.create_task(container.create_item(item)) for item in batch]
     )
     print(f"Batch of {len(batch)} items done!")
